194cf96e812186642d53bf2fe87d269e18c79f54
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-24 14:14:43 -0300

    Add random generated id.

--- person.rb
+++ person.rb
@@ -2,8 +2,8 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', _parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', _parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
   end
