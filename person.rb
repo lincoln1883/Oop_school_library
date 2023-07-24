933062ae5baa50c6ad2f31df98dcec426c4fb7ce
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-24 17:53:01 -0300

    Add inheritance from nameable to person class with correct name method.

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', _parent_permission: true)
+    super()
     @id = rand(1..1000)
     @name = name
     @age = age
@@ -18,5 +20,9 @@ class Person
 
   def can_use_services?(parent_permission)
     @age >= 18 || parent_permission
+  end
+
+  def correct_name
+    name
   end
 end
