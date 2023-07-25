a5d4ff7417131e916ddcac829af5a2fbd7a4db01
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-25 02:59:48 -0300

    Create many to many relationship with person through rentals.

--- book.rb
+++ book.rb
@@ -1,8 +1,10 @@
+require_relative 'rental'
 class Book
-  attr_accessor :author, :title
+  attr_accessor :author, :title, :rental
 
-  def initialize(title, author)
+  def initialize(title, author, rental)
     @title = title
     @author = author
+    @rental = rental
   end
 end
