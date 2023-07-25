284fe50a130e078f76ffaadd6f1e87da8e658e69
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-25 01:11:09 -0300

    Create the belongs-to side of student classroom (a student belongs to classroom.

--- student.rb
+++ student.rb
@@ -1,15 +1,18 @@
 require_relative 'person'
 class Student < Person
-  attr_accessor :classroom
-
   def initialize(age, name, classroom, parent_permission)
     super(age, name)
     @id = id
-    @classroom = classroom
+    @classroom = Classroom.new(classroom)
     @parent_permission = parent_permission
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
