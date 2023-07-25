b88536b347c71a20129b62d91555ebcf428fef6e
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-25 18:31:18 -0300

    Create the switch statements with valid options(methods) for the user.

--- app.rb
+++ app.rb
@@ -6,22 +6,66 @@ require_relative 'rental'
 require_relative 'classroom'
 
 class App
+  def list_all_books
+    puts 'list all books'
+  end
+
+  def list_all_people
+    puts 'list all people'
+  end
+
+  def create_a_person
+    puts 'create a person'
+  end
+
+  def create_a_book
+    puts 'create a book'
+  end
+
+  def create_a_rental
+    puts 'rent a book'
+  end
+
+  def list_all_rentals
+    puts 'list all books you have in rental'
+  end
+
+  def main_menu
+    puts 'Please choose an option by entering a number: '
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a book'
+    puts '4 - Create a person'
+    puts '5 - Create a rental'
+    puts '6 - List all rental for a given person id'
+    puts '7 - exit'
+  end
+
+  def user_choice(choice)
+    case choice
+    when 1
+      list_all_books
+    when 2
+      list_all_people
+    when 3
+      create_a_book
+    when 4
+      create_a_person
+    when 5
+      create_a_rental
+    when 6
+      list_all_rentals
+    else puts 'Exiting the program...'
+         exit
+    end
+  end
+
   def menu
+    puts ' Welcome to School Library App! '.center(50, '#'), "\n"
     loop do
-      puts ' Welcome to School Library App! '.center(50, '#'), "\n"
-
-      puts 'Please choose an option by entering a number: '
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a book'
-      puts '4 - Create a person'
-      puts '5 - Create a rental'
-      puts '6 - List all rental for a given person id'
-      puts '7 - exit'
-      choice = gets.chomp
-
-      break unless choice != '7'
+      main_menu
+      choice = gets.chomp.to_i
+      user_choice(choice) || 7
     end
   end
 end
-
