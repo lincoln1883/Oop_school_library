3d243861c36faefa1e68393a943591484479e60c
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-31 13:29:27 -0300

    Remove date input references.

--- app.rb
+++ app.rb
@@ -94,11 +94,7 @@ class App
     end
     person_index = gets.chomp.to_i
     puts "You selected person index: #{person_index}"
-    puts 'Please enter a date: '
-    print 'Date: '
-    date = gets.chomp
-    puts "You entered date: #{date}"
-    rental = Rental.new(date, @books[book_index], @people[person_index])
+    rental = Rental.new(@books[book_index], @people[person_index])
     @rentals << rental
     puts 'Successfully rented a book.'
     puts
@@ -116,45 +112,6 @@ class App
         puts 'You currently have no rented books!'
       end
       puts
-    end
-  end
-
-  def main_menu
-    puts 'Please choose an option by entering a number: '
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a book'
-    puts '4 - Create a person'
-    puts '5 - Create a rental'
-    puts '6 - List all rental for a given person id'
-    puts '7 - exit'
-  end
-
-  def user_choice(choice)
-    case choice
-    when 1
-      list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_a_book
-    when 4
-      create_a_person
-    when 5
-      create_a_rental
-    when 6
-      list_all_rentals
-    else puts 'Exiting the program...'
-         exit
-    end
-  end
-
-  def menu
-    puts ' Welcome to School Library App! '.center(50, '#'), "\n"
-    loop do
-      main_menu
-      choice = gets.chomp.to_i
-      user_choice(choice) || 7
     end
   end
 end
