907de530c881e7176da221e13c3b8a6d056411c6
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-05 19:21:34 -0300

    Modify initializer to override parent permission and gain global access.

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require_relative 'person'
+require_relative 'classroom'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name, parent_permission)
-    super(age, name)
+  def initialize(age, name, classroom, parent_permission)
+    super(age, name, parent_permission)
     @id = id
-    @classroom = []
-    @parent_permission = parent_permission
+    @classroom = classroom
   end
 
   def play_hooky
@@ -16,5 +16,9 @@ class Student < Person
   def add_to_classroom(classroom)
     @classroom = classroom
     classroom.students << self unless classroom.students.include?(self)
+  end
+
+  def parent_permission
+    can_use_services?
   end
 end
