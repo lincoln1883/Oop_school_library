7c34f70446e87797e178a3b739f88a5f20c6d316
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-05 19:23:22 -0300

    Modify initializer to override parent permission and gain global access.

--- teacher.rb
+++ teacher.rb
@@ -2,15 +2,13 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, name, specialization)
-    super(age, name)
+  def initialize(age, name, specialization, parent_permission: true)
+    super(age, name, parent_permission)
     @id = id
-    @age = age
-    @name = name
     @specialization = specialization
   end
 
   def can_use_services?
-    super parent_permission: true
+    true
   end
 end
