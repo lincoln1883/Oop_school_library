4393e13b8abb0afe97e52135e4a26ebcc55f6d0a
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-06 14:31:02 -0300

    Create load rentals method.

--- file_load.rb
+++ file_load.rb
@@ -1,5 +1,6 @@
 require_relative 'book'
 require_relative 'person'
+require_relative 'rental'
 require 'json'
 
 def load_books
@@ -8,9 +9,11 @@ def load_books
   rescue StandardError
     []
   end
+  books = []
   stored_books.map do |book_data|
-    Book.new(book_data['title'], book_data['author'])
+    books << Book.new(book_data['title'], book_data['author'])
   end
+  books
 end
 
 def load_person
@@ -19,12 +22,36 @@ def load_person
   rescue StandardError
     []
   end
-
   @people = stored_people.map do |person|
     if person.key?('classroom')
-      Student.new(person['age'], person['name'], person['classroom'], person['parent_permission'])
+      Student.new(person['id'], person['age'], person['name'], person['classroom'], person['parent_permission'])
     elsif person.key?('specialization')
-      Teacher.new(person['age'], person['name'], person['specialization'])
+      Teacher.new(person['id'], person['age'], person['name'], person['specialization'])
     end
   end
 end
+
+def load_rentals
+  stored_rentals = begin
+    JSON.parse(File.read('rental.json'))
+  rescue StandardError => e
+    puts "Error reading 'rental.json': #{e.message}"
+    []
+  end
+  @rentals = stored_rentals.map do |rental|
+    date = rental['date']
+    person_id = rental['person']['id']
+    person = @people.find { |p| p.id == person_id }
+
+    unless person
+      person = Person.new(rental['person']['age'], rental['person']['name'])
+      @people << person
+    end
+
+    book = Book.new(rental['book']['title'], rental['book']['author'])
+    Rental.new(date, book, person)
+  end
+end
+
+
+
