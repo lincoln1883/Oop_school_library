6a74baf6f31fdc10f14c45a787a60a0d578e9a1e
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-06 14:27:22 -0300

    Modify initializer and add id generator method.

--- person.rb
+++ person.rb
@@ -1,11 +1,10 @@
 require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
+  attr_accessor :name, :age, :rentals, :id
 
-  def initialize(age, name, parent_permission)
+  def initialize(id, age, name: 'Unknown', parent_permission: true)
     super()
-    @id = rand(1..1000)
+    @id = id || generate_id
     @age = age
     @name = name
     @rentals = []
@@ -18,6 +17,10 @@ class Person < Nameable
     @age >= 18
   end
 
+  def generate_id
+    rand(1..100)
+  end
+
   public
 
   def can_use_services?
@@ -28,7 +31,7 @@ class Person < Nameable
     @name
   end
 
-  def rent_a_book(date, person)
-    Rental.new(date, self, person)
+  def rent_a_book(date, book)
+    Rental.new(date, book, self)
   end
 end
