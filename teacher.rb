a53933cf878574331c96d019a419b47fc26bd171
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-06 13:48:43 -0300

    Modify initializer.

--- teacher.rb
+++ teacher.rb
@@ -2,9 +2,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, name, specialization, parent_permission: true)
-    super(age, name, parent_permission)
-    @id = id
+  def initialize(id, age, name, specialization, parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
